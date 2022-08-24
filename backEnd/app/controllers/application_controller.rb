46e0ae2fc007c68d03a812f049c46a8b05d7524f
JaiVon <frederickjaivon@yahoo.com>
2022-08-24 15:59:27 -0400

    Updates

--- backEnd/app/controllers/application_controller.rb
+++ backEnd/app/controllers/application_controller.rb
@@ -1,4 +1,32 @@
 class ApplicationController < ActionController::API
+  before_action :authenticate_user
   include ActionController::Cookies
+
+  # goals 
+  # only Admin can access bulletin
+  # Member can view bulletin
+  # Admin can view bulletin
+  # Admin can update, post, delete bulletin
+
+  # Admin belongs_to bulletins
+  # Admin belongs_to members
+
+  # todays goals 8/23/2022
+  # Set up client side routes
+  # Set up Admin, member, bulletin, relationships
+  # Set up Admin authentication
+  # Set up Member authentication
+
+  # controller action for signup, login and logout
+  # Individual sessions for member and admin
+  # 
+
+  def current_user
+    @current_user ||= Member.find_by_id(session[:member_id])
+  end
+
+  def authenticate_user 
+    render json: {errors: "not authorized"}, status: :unauthorized unless current_user
+  end
 
 end
--- backEnd/db/seeds.rb
+++ backEnd/db/seeds.rb
@@ -5,3 +5,45 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Admin.destroy_all
+Member.destroy_all
+Bulletin.destroy_all # why don't these delete my previous seed!!!
+
+
+puts '🌱 seeding admins 👮‍♂️'
+
+# t.string :name
+# t.string :username
+# t.string :password
+# t.string :email
+# t.integer :member_id
+# t.integer :bulletin_id
+
+
+ad1 = Admin.create(name: "Charles", username: "CharlesDW", password: "123Admin", email: "CharlesDW@gmail.com")
+# ad2 = Admin.create(name: "Martha", username: "MarthaWithers", password: "321Admin", email: "MarthaWithers@gmail.com")
+
+puts '🌱 seeding members 👨‍👩‍👦‍👦👨‍👩‍👧‍👧👨‍👩‍👧‍👦'
+
+# t.string :name
+# t.string :username
+# t.string :password
+# t.string :email
+
+m1 = Member.create(name: "Bill", username: "BillTT", password: "first", email: "BillTurner@gmail.com")
+m2 = Member.create(name: "Kelly", username: "KellyCrane", password: "second", email: "KellyCrane@gmail.com")
+m3 = Member.create(name: "Vanessa", username: "VanessAdler", password: "third", email: "VanessaAdler@gmail.com")
+m4 = Member.create(name: "Francis", username: "Frank", password: "fourth", email: "FrankTizic@gmail.com")
+
+puts '🌱 seeding bulletins 📆'
+
+# t.string :name
+# t.string :date
+# t.string :activity
+
+b1 = Bulletin.create(name: "Morning Prayer", date: "8/30/2022", activity: "Regular Service" , member_id: m1.id, admin_id: ad1.id)
+b2 = Bulletin.create(name: "Community Gardening", date: "9/1/2022", activity: "Community Service", member_id: m2.id, admin_id: ad1.id)
+b3 = Bulletin.create(name: "Community Jungle Gym Fundraiser", date: "Ongoing", activity: "Community Service", member_id: m3.id, admin_id: ad1.id)
+b4 = Bulletin.create(name: "Late Service", date: "8/26/2022", activity: "Regular Service", member_id: m4.id, admin_id: ad1.id)
+b5 = Bulletin.create(name: "Weekend Potluck", date: "9/16/2022", activity: "Outside Service", member_id: m3.id, admin_id: ad1.id)
