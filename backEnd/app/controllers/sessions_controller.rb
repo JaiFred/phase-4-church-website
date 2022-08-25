da0aca54f67b6deae8768b7a806042135fe47e74
JaiVon <frederickjaivon@yahoo.com>
2022-08-25 13:49:48 -0400

    Added CRUD to Controllers

---
+++ backEnd/app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+
+    # POST '/login'
+    def create
+        member = Member.find_by(username: params[:username])
+        if member&.authenticate(params[:password])
+            session[:member_id] = member.id
+            render json: member, status: :ok
+        else
+            render json: { errors: 'Invalid credentials' }, status: :unauthorized
+        end
+    end
+
+    # DELETE '/logout'
+    def destroy
+        if current_member
+            session.clear
+        else
+            render json: { errors: 'No active session'}, status: :unauthorized
+        end
+    end
+end
--- backEnd/config/routes.rb
+++ backEnd/config/routes.rb
@@ -3,7 +3,15 @@ Rails.application.routes.draw do
   resources :bulletins
   resources :members
   resources :admins
+  
+  get "/me", to: "members#show"
+  post "/signup", to: "members#create"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+  
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+
+  
 end
