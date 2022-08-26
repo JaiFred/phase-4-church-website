71e0b453c11ace49dc731d430d4e687c14c14d6a
JaiVon <frederickjaivon@yahoo.com>
2022-08-26 14:15:45 -0400

    React Fixxed Slightly

--- frontEnd/src/components/GroupDetail.js
+++ frontEnd/src/components/GroupDetail.js
@@ -1,11 +1,9 @@
 import React, { useState, useEffect, useCallback } from 'react'
 import { Link } from 'react-router-dom'
-import { Navbar } from './Navbar.js'
 
 function GroupDetail({ groupId, leaveGroup, joinGroup }) {
   return(
     <div>
-      <Navbar/>
     </div>
   )
 }
